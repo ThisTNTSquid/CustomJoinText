cd9b74096c97460b1df0753e48767ed55efd9e34
ThisTNTSquid <tntsquid@corecrafted.net>
2017-06-19 00:20:53 +0800

    Add permission support

--- src/net/corecrafted/cjt/JoinLeaveListener.java
+++ src/net/corecrafted/cjt/JoinLeaveListener.java
@@ -14,19 +14,30 @@ public class JoinLeaveListener implements Listener {
 
     JoinTextMain plugin;
     FileConfiguration config;
+    String joinMsgPermissions;
+    String leaveMsgPermisisons;
 
     public JoinLeaveListener(JoinTextMain plugin) {
         this.plugin = plugin;
         config = plugin.getConfig();
+        joinMsgPermissions = config.getString("join-message-permissions");
+        leaveMsgPermisisons = config.getString("leave-message-permissions");
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         if (config.getBoolean("enable-join-message")){
             String playerName = getDisplayName(e.getPlayer());
-            String message = ChatColor.translateAlternateColorCodes('&', config.getString("join-message").replaceAll("%player%", playerName));
-            e.setJoinMessage(message);
-            plugin.getConsole().sendMessage(message);
+            if (joinMsgPermissions!=null){
+                if (e.getPlayer().hasPermission(joinMsgPermissions)){
+                    sendJoinMsg(e,playerName);
+                } else
+                    return;
+
+            } else {
+                sendJoinMsg(e,playerName);
+            }
+
         } else
             return;
 
@@ -35,9 +46,15 @@ public class JoinLeaveListener implements Listener {
     public void onLeave(PlayerQuitEvent e) {
         if (config.getBoolean("enable-leave-message")) {
             String playerName = getDisplayName(e.getPlayer());
-            String message = ChatColor.translateAlternateColorCodes('&', config.getString("leave-message").replaceAll("%player%", playerName));
-            e.setQuitMessage(message);
-            plugin.getConsole().sendMessage( message);
+            if (leaveMsgPermisisons!=null){
+                if(e.getPlayer().hasPermission(leaveMsgPermisisons)){
+                    sendLeaveMsg(e,playerName);
+                } else
+                    return;
+            } else {
+                sendLeaveMsg(e,playerName);
+            }
+
         } else
             return;
     }
@@ -50,5 +67,17 @@ public class JoinLeaveListener implements Listener {
         } else {
             return p.getName();
         }
+    }
+
+    private void sendJoinMsg(PlayerJoinEvent e,String playerName){
+        String message = ChatColor.translateAlternateColorCodes('&', config.getString("join-message").replaceAll("%player%", playerName));
+        e.setJoinMessage(message);
+        plugin.getConsole().sendMessage(message);
+    }
+
+    private void sendLeaveMsg(PlayerQuitEvent e,String playerName){
+        String message = ChatColor.translateAlternateColorCodes('&', config.getString("leave-message").replaceAll("%player%", playerName));
+        e.setQuitMessage(message);
+        plugin.getConsole().sendMessage( message);
     }
 }
