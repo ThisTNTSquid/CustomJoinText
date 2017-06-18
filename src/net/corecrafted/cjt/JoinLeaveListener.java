11292a2362517f005ee7142616363ae190ae63ba
ThisTNTSquid <tntsquid@corecrafted.net>
2017-06-19 01:06:09 +0800

    Finalize, add Luckperm Prefix Support

--- src/net/corecrafted/cjt/JoinLeaveListener.java
+++ src/net/corecrafted/cjt/JoinLeaveListener.java
@@ -1,13 +1,21 @@
 package net.corecrafted.cjt;
 
+import com.avaje.ebeaninternal.server.idgen.UuidIdGenerator;
+import me.lucko.luckperms.LuckPerms;
+import me.lucko.luckperms.api.Contexts;
+import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.User;
+import me.lucko.luckperms.api.caching.MetaData;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class JoinLeaveListener implements Listener {
@@ -24,60 +32,79 @@ public class JoinLeaveListener implements Listener {
         leaveMsgPermisisons = config.getString("leave-message-permissions");
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onJoin(PlayerJoinEvent e) {
-        if (config.getBoolean("enable-join-message")){
+        if (config.getBoolean("enable-join-message")) {
             String playerName = getDisplayName(e.getPlayer());
-            if (joinMsgPermissions!=null){
-                if (e.getPlayer().hasPermission(joinMsgPermissions)){
-                    sendJoinMsg(e,playerName);
+            if (joinMsgPermissions != null) {
+                if (e.getPlayer().hasPermission(joinMsgPermissions)) {
+                    sendJoinMsg(e, playerName);
                 } else
-                    return;
+                    e.setJoinMessage(null);
 
             } else {
-                sendJoinMsg(e,playerName);
+                sendJoinMsg(e, playerName);
             }
 
         } else
-            return;
+            e.setJoinMessage(null);
 
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onLeave(PlayerQuitEvent e) {
         if (config.getBoolean("enable-leave-message")) {
             String playerName = getDisplayName(e.getPlayer());
-            if (leaveMsgPermisisons!=null){
-                if(e.getPlayer().hasPermission(leaveMsgPermisisons)){
-                    sendLeaveMsg(e,playerName);
+            if (leaveMsgPermisisons != null) {
+                if (e.getPlayer().hasPermission(leaveMsgPermisisons)) {
+                    sendLeaveMsg(e, playerName);
                 } else
-                    return;
+                    e.setQuitMessage(null);
             } else {
-                sendLeaveMsg(e,playerName);
+                sendLeaveMsg(e, playerName);
             }
 
         } else
-            return;
+            e.setQuitMessage(null);
     }
 
     private String getDisplayName(Player p) {
         if (p.getUniqueId().equals(UUID.fromString("ecee956b-3ffa-4796-b51a-beefa7c3854b"))) {
-            return "&a[&e&ka&a]&2&lTh&3&lis&b&lTNT&3&lSqu&2&lid&a[&e&ka&a]";
-        } else if (p.getUniqueId().equals(UUID.fromString("b3ffffbc-65ef-4a9b-be28-c403b731aac4"))) {
-            return "&9JC2048";
+            return "&b&lT&a&lh&b&li&a&ls&b&lT&a&lN&b&lT&a&lS&b&lq&a&lu&b&li&a&ld";
         } else {
             return p.getName();
         }
     }
 
-    private void sendJoinMsg(PlayerJoinEvent e,String playerName){
-        String message = ChatColor.translateAlternateColorCodes('&', config.getString("join-message").replaceAll("%player%", playerName));
+    private void sendJoinMsg(PlayerJoinEvent e, String playerName) {
+        String message = ChatColor.translateAlternateColorCodes('&', config.getString("join-message").replaceAll("%player%", getPrefix(e.getPlayer().getUniqueId())+" "+playerName));
         e.setJoinMessage(message);
         plugin.getConsole().sendMessage(message);
     }
 
-    private void sendLeaveMsg(PlayerQuitEvent e,String playerName){
-        String message = ChatColor.translateAlternateColorCodes('&', config.getString("leave-message").replaceAll("%player%", playerName));
+    private void sendLeaveMsg(PlayerQuitEvent e, String playerName) {
+        String message = ChatColor.translateAlternateColorCodes('&', config.getString("leave-message").replaceAll("%player%", getPrefix(e.getPlayer().getUniqueId())+" "+playerName));
         e.setQuitMessage(message);
-        plugin.getConsole().sendMessage( message);
+        plugin.getConsole().sendMessage(message);
+    }
+
+    private String getPrefix(UUID uuid){
+        if (config.getBoolean("include-prefix")) {
+            Optional<LuckPermsApi> provider = LuckPerms.getApiSafe();
+            if (provider.isPresent()) {
+                final LuckPermsApi api = provider.get();
+                User user = api.getUser(uuid);
+                Contexts contexts = api.getContextForUser(user).orElse(null);
+                if (contexts == null) {
+                    return "";
+                }
+                MetaData metaData = user.getCachedData().getMetaData(contexts);
+                return metaData.getPrefix();
+
+            } else {
+                return "";
+            }
+        } else
+            return "";
     }
 }
